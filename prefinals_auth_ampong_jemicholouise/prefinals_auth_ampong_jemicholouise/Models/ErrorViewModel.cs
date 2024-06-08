3f78dbc4e2666425f4833fe51f80d114a3266423
Ampong_JemichoLouise_BSIT32E2 <160830705+AmpongJemichoLouiseBSIT@users.noreply.github.com>
2024-06-08 13:08:31 +0800

    Wala pang laman

---
+++ prefinals_auth_ampong_jemicholouise/prefinals_auth_ampong_jemicholouise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prefinals_auth_ampong_jemicholouise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
